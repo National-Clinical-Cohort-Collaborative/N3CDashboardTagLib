ccda0a81db01b6d0f48f458eb031eec6cb21c5b9
eichmann <david-eichmann@uiowa.edu>
2023-04-13 18:20:30 +0000

    Added additional columns to feed status. Also download/copy buttons, although they're not showing.

--- src/non-packaged-resources/monitoring.sql
+++ src/non-packaged-resources/monitoring.sql
@@ -19,16 +19,25 @@ where foo.file not in (select file from n3c_dashboard.feed_no_updates )
 order by 1,2;
 
 create view n3c_dashboard.feed_deltas as
-select foo.file,foo.updated,foo.rows - bar.rows as row_delta, foo.count - bar.count as count_delta
+select foo.file,foo.updated,foo.rows - bar.rows as row_delta, bar.count as previous_count, foo.count as current_count, foo.count - bar.count as count_delta
 from n3c_dashboard.feed_updates as foo, n3c_dashboard.feed_updates as bar
 where foo.file = bar.file
   and foo.updated > bar.updated
 ;
 
 create view n3c_dashboard.feed_status as
-select file, to_char(fetched, 'YYYY-MM-DD') as fetched, to_char(updated, 'YYYY-MM-DD') as updated, now() - updated as interval, row_delta, count_delta
+select
+	rid,
+	file,
+	to_char(fetched, 'YYYY-MM-DD') as fetched,
+	to_char(updated, 'YYYY-MM-DD') as updated,
+	now() - updated as interval,
+	row_delta,
+	previous_count,
+	current_count,
+	count_delta
 from
-	(select file,updated as fetched,(((metadata->>'modified')::jsonb)->>'time')::timestamp as updated from palantir.tiger_team_file_new) as foo
+	(select rid, file,updated as fetched,(((metadata->>'modified')::jsonb)->>'time')::timestamp as updated from palantir.tiger_team_file_new) as foo
 natural left outer join
 	(select * from n3c_dashboard.feed_deltas) as bar
 ;
